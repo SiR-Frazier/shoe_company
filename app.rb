14c2721f5707d4a6532c283fbd15a8b3a7d767c9
Bryce Frazier <bx.frazier@gmail.com>
2018-03-02 11:46:37 -0800

    update post for shoes. unable to view shoe names after their added

--- app.rb
+++ app.rb
@@ -9,6 +9,7 @@ require('./lib/shoe.rb')
 require('./lib/store.rb')
 
 get('/') do
+  @stores = Store.all()
   erb(:home)
 end
 
@@ -18,6 +19,27 @@ get('/stores') do
 end
 
 get('/shoes') do
+  @shoes = Shoe.all()
+  erb(:shoes)
+end
+
+post('/stores') do
+  name = params[:name]
+  @store = Store.create({:name => name})
+  @stores = Store.all()
+  erb(:stores)
+end
+
+get('/stores/:id') do
+  name = params[:name]
+  @store = Store.find(params.fetch("id").to_i())
+  erb(:store_info)
+end
+
+post('/shoes') do
+  brand = params[:brand]
+  price = params[:price]
+  @shoe = Shoe.create({:brand => brand, :price => price})
   @shoes = Shoe.all()
   erb(:shoes)
 end
