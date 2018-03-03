40865d9689e9f8ae5291954bd7c78c0f29e2cceb
Bryce Frazier <bx.frazier@gmail.com>
2018-03-02 16:24:52 -0800

    2 passing specs for store.rb

---
+++ spec/store_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe(Store) do
+  it("checks to make sure name field is not empty") do
+    store = Store.new({:name => ""})
+    expect(store.save()).to(eq(false))
+  end
+
+  it("ensures that the length of the brand is at most 100 characters") do
+    store =Store.new({:name => "x".*(101)})
+    expect(store.save()).to(eq(false))
+  end
+
+end
