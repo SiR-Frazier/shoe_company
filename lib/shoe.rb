5dde1c29ee690b72b70ae0277f45197358bad593
sir-frazier <bxfrazier@gmail.com>
2018-03-05 19:43:04 -0800

    add currency_converter method to shoe.rb

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,13 +2,15 @@ class Shoe < ActiveRecord::Base
   has_and_belongs_to_many :stores
   validates(:brand, {:presence => true, :length => { :maximum => 100}})
   validates(:price, :presence => true)
-  before_save(:capitalize)
-  
+  before_save(:capitalize, :currency_converter)
+
   private
 
   def capitalize
     self.brand = brand.gsub(/\w+/, &:capitalize)
   end
 
-
+  def currency_converter
+    self.brand = brand.concat(".00")
+  end
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,4 +1,12 @@
 class Store < ActiveRecord::Base
   has_and_belongs_to_many :shoes
   validates(:name, {:presence => true, :length => {:maximum => 100}})
+  before_save(:capitalize)
+
+  private
+
+  def capitalize
+    self.name = name.gsub(/\w+/, &:capitalize)
+  end
+
 end
