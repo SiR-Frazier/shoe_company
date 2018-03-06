c1594062815272c33621141eddf45c471a5f91e2
sir-frazier <bxfrazier@gmail.com>
2018-03-05 19:31:20 -0800

    add capitalize method to shoe.rb

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,4 +2,13 @@ class Shoe < ActiveRecord::Base
   has_and_belongs_to_many :stores
   validates(:brand, {:presence => true, :length => { :maximum => 100}})
   validates(:price, :presence => true)
+  before_save(:capitalize)
+  
+  private
+
+  def capitalize
+    self.brand = brand.gsub(/\w+/, &:capitalize)
+  end
+
+
 end
